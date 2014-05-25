b4db24f3e2c98ce9399911b7362c38eff7b6bd89
siubawong <jcheung@jcheungs-MacBook-Pro.local>
2014-05-25 15:58:01 -0700

    testing commit to remote master

--- CardGame/Model/Deck.h
+++ CardGame/Model/Deck.h
@@ -5,6 +5,7 @@
 //  Created by Jonathan Wong on 3/30/14.
 //  Copyright (c) 2014 Jonathan Wong. All rights reserved.
 //
+//this is a test commit
 
 #import <Foundation/Foundation.h>
 #import "Card.h"
